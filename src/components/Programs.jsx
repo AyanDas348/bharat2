d79186c8101ab7c240c2c070df0f1393c28364d3
Ayan Das <dasayan348@gmail.com>
2023-12-31 18:54:24 +0530

    A

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -33,7 +33,7 @@ const Programs = () => {
                         <button
                             className="w-full mt-4 h-12 text-black font-semibold 
                             bg-gradient-to-r from-indigo-500 via-purple-500
-                             to-pink-500rounded-lg shadow-lg hover:scale-105 duration-200 
+                             to-pink-500 rounded-lg shadow-lg hover:scale-105 duration-200 
                              hover:drop-shadow-2xl hover:shadow-[#7dd3fc] hover:cursor-pointer"
                         >
                             Get Started
